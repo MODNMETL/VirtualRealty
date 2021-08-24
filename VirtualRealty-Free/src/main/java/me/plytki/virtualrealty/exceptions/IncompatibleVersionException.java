991b3db921122c465ce76d58823064f2edb0d05a
MODNMETL <89078751+MODNMETL@users.noreply.github.com>
2021-08-24 18:51:42 +0100

    Delete VirtualRealty-Free directory

--- VirtualRealty-Free/src/main/java/me/plytki/virtualrealty/exceptions/IncompatibleVersionException.java
+++
@@ -1,9 +0,0 @@
-package me.plytki.virtualrealty.exceptions;
-
-public class IncompatibleVersionException extends Exception {
-
-    public IncompatibleVersionException(String exception) {
-        super(exception);
-    }
-
-}
