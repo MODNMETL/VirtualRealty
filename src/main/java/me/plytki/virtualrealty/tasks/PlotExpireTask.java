c7738a1e7caa5c42f2d3f28a0506600b2c68fb0a
MODNMETL <89078751+MODNMETL@users.noreply.github.com>
2021-08-24 18:54:55 +0100

    Add files via upload

---
+++ src/main/java/me/plytki/virtualrealty/tasks/PlotExpireTask.java
@@ -0,0 +1,15 @@
+package me.plytki.virtualrealty.tasks;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class PlotExpireTask extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        Bukkit.getOnlinePlayers().forEach(player -> {
+
+        });
+    }
+
+}
